7eda083abbf1f100116225d6aca6538f8be2b70d
test <3035331591@qq.com>
2022-11-07 16:22:52 +0800

    第二次提交

---
+++ JAVA/day6/p89.java
@@ -0,0 +1,13 @@
+﻿/*
+ * @Author: test 3035331591@qq.com
+ * @Date: 2022-11-07 16:19:33
+ * @LastEditors: test 3035331591@qq.com
+ * @LastEditTime: 2022-11-07 16:19:38
+ * @FilePath: \JAVA\day6\p89.java
+ * @Description:
+ *
+ * Copyright (c) 2022 by test 3035331591@qq.com, All Rights Reserved.
+ */
+public class p89 {
+
+}
