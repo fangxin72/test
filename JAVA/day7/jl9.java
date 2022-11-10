e7f3270e10ec05b01ac8765d582e01dd8890de33
test <3035331591@qq.com>
2022-11-10 15:08:12 +0800

    java

--- JAVA/day7/jl9.java
+++ JAVA/day7/jl9.java
@@ -1,8 +1,11 @@
-﻿/*
+﻿import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+import java.util.*;
+
+/*
  * @Author: Yilsi
  * @Date: 2022-11-10 14:18:05
  * @LastEditors: Fx
- * @LastEditTime: 2022-11-10 14:25:58
+ * @LastEditTime: 2022-11-10 15:07:03
  * @FilePath: \JAVA\day7\jl9.java
  * @Description: I don't know.
  * Cuius rei demonstrationem mirabilem sane detexi. Hanc marginis exiguitas non caperet.
@@ -11,12 +14,32 @@
 public class jl9 {
   public static void main(String[] args) {
     int scores[];
+
     scores = new int[5];
     scores[0] = 10;
     scores[1] = 20;
     scores[2] = 30;
     scores[3] = 40;
     scores[4] = 50;
-    System.out.println(scores[0]);
+    System.out.println(scores[3]);
+
+    // 声明数组
+    int[] score2 = { 10, 20, 30, 40, 50 };
+    int[] score3 = new int[] { 10, 20, 30, 40, 50 };
+    // []不能写上长度
+    System.out.println(score2[0]);
+
+    System.out.println(score3[2]);
+    System.out.println("scores的数组长度是" + scores.length);
+
+    Scanner input = new Scanner(System.in);
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println("请输入数组中下标位置" + i + "的数组元素");
+      scores[i] = input.nextInt();
+    }
+
+    for (int i = 0; i < score2.length; i++) {
+      System.out.println(scores[i]);
+    }
   }
 }
