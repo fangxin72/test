b9f0b9fb9be8701e8e0d6b98cc087c9378c51a10
test <3035331591@qq.com>
2022-11-10 14:16:29 +0800

    20

--- JAVA/zuoye/t1.java
+++ JAVA/zuoye/t1.java
@@ -1,4 +1,14 @@
-﻿public class t1{
+﻿/*
+ * @Author: Yilsi
+ * @Date: 2022-10-29 07:07:54
+ * @LastEditors: Fx
+ * @LastEditTime: 2022-11-10 14:16:02
+ * @FilePath: \JAVA\zuoye\t1.java
+ * @Description: I don't know.
+ * Cuius rei demonstrationem mirabilem sane detexi. Hanc marginis exiguitas non caperet.
+ * Copyright (c) 2022 by Fx, All Rights Reserved.
+ */
+public class t1 {
     public static void main(String[] args) {
         System.out.println("10和8交换");
         int a = 10;
@@ -6,9 +16,9 @@
         int c;
         System.out.println("左手为：" + a);
         System.out.println("右手为：" + b);
-        c=b;
-        b=a;
-        a=c;
+        c = b;
+        b = a;
+        a = c;
         System.out.println("交换结果左手为：" + a);
         System.out.println("交换结果右手为: " + b);
     }
