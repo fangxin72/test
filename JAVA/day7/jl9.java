bb27329614be2b1b567be6d64291481e31a1cac3
test <3035331591@qq.com>
2022-11-10 14:28:11 +0800

    qwertyui

---
+++ JAVA/day7/jl9.java
@@ -0,0 +1,22 @@
+﻿/*
+ * @Author: Yilsi
+ * @Date: 2022-11-10 14:18:05
+ * @LastEditors: Fx
+ * @LastEditTime: 2022-11-10 14:25:58
+ * @FilePath: \JAVA\day7\jl9.java
+ * @Description: I don't know.
+ * Cuius rei demonstrationem mirabilem sane detexi. Hanc marginis exiguitas non caperet.
+ * Copyright (c) 2022 by Fx, All Rights Reserved.
+ */
+public class jl9 {
+  public static void main(String[] args) {
+    int scores[];
+    scores = new int[5];
+    scores[0] = 10;
+    scores[1] = 20;
+    scores[2] = 30;
+    scores[3] = 40;
+    scores[4] = 50;
+    System.out.println(scores[0]);
+  }
+}
